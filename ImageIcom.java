01cf1b28c78cb1ad131ac3ca714a607847deaa29
Anjali Karki <166734253+anjaalliiiii@users.noreply.github.com>
2024-04-18 23:21:18 +0530

    Add files via upload

---
+++ ImageIcom.java
@@ -0,0 +1,17 @@
+package projects.game2;
+
+import java.awt.Image;
+import java.net.URL;
+
+public class ImageIcom {
+
+    public ImageIcom(URL resource) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public Image getImage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getImage'");
+    }
+
+}
